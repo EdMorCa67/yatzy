d37487319eb2bfcf43c0d4c8087a9bc062bf2b88
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-01-21 14:21:10 +0100

    refactor(tests): Refactor tests for sum_ones/twos/threes/fours/fives/sixes

--- test/test_yatzy.py
+++ test/test_yatzy.py
@@ -4,11 +4,11 @@ import pytest
 # These unit tests can be run using the py.test framework
 # available from http://pytest.org/
 
-def test_scores_sum_of_all_dice():
+def test_chance_scores_sum_of_all_dice():
     expected = 15
-    actual = Yatzy.scores_sum_of_all_dice(2, 3, 4, 5, 1)
+    actual = Yatzy.chance_scores_sum_of_all_dice(2, 3, 4, 5, 1)
     assert expected == actual
-    assert 16 == Yatzy.scores_sum_of_all_dice(3, 3, 4, 5, 1)
+    assert 16 == Yatzy.chance_scores_sum_of_all_dice(3, 3, 4, 5, 1)
 
 @pytest.mark.parametrize(
     "dices, expected_result",
@@ -22,51 +22,104 @@ def test_yatzy_scores_fifty(dices, expected_result):
     assert Yatzy.yatzy(dices) == expected_result
 
 
-def test_sum_ones():
-    assert Yatzy.ones(1, 2, 3, 4, 5) == 1
-    assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
-    assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
-    assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([1, 2, 1, 4, 5], 2),
+        ([6, 2, 2, 4, 5], 0),
+        ([1, 2, 1, 1, 1], 4)
+    ],
+)
+def test_sum_ones(dices, expected_result):
+    assert Yatzy.sum_ones(*dices) == expected_result
 
 
-def test_sum_twos():
-    assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
-    assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
+
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([1, 2, 3, 2, 6], 4),
+        ([2, 2, 2, 2, 2], 10),
+        ([1, 2, 1, 1, 1], 2)
+    ],
+)
+def test_sum_twos(dices, expected_result):
+    assert Yatzy.sum_twos(*dices) == expected_result
 
 
-def test_sum_threes():
-    assert 6 == Yatzy.threes(1, 2, 3, 2, 3)
-    assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
 
 
-def test_sum_fours():
-    assert 12 == Yatzy(4, 4, 4, 5, 5).fours()
-    assert 8 == Yatzy(4, 4, 5, 5, 5).fours()
-    assert 4 == Yatzy(4, 5, 5, 5, 5).fours()
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([1, 2, 3, 2, 3], 6),
+        ([2, 3, 3, 3, 3], 12),
+        ([1, 2, 1, 1, 1], 0)
+    ],
+)
+def test_sum_threes(dices, expected_result):
+    assert Yatzy.sum_threes(*dices) == expected_result
 
 
-def test_sum_fives():
-    assert 10 == Yatzy(4, 4, 4, 5, 5).fives()
-    assert 15 == Yatzy(4, 4, 5, 5, 5).fives()
-    assert 20 == Yatzy(4, 5, 5, 5, 5).fives()
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([4, 4, 5, 5, 5], 8),
+        ([4, 4, 4, 5, 5], 12),
+        ([4, 5, 5, 5, 5], 4)
+    ],
+)
+def test_sum_fours(dices, expected_result):
+    assert Yatzy.sum_fours(*dices) == expected_result
+
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([4, 4, 3, 5, 5], 10),
+        ([4, 4, 5, 5, 5], 15),
+        ([4, 5, 5, 5, 5], 20)
+    ],
+)
+def test_sum_fives(dices, expected_result):
+    assert Yatzy.sum_fives(*dices) == expected_result
 
 
-def test_sum_sixes():
-    assert 0 == Yatzy(4, 4, 4, 5, 5).sixes()
-    assert 6 == Yatzy(4, 4, 6, 5, 5).sixes()
-    assert 18 == Yatzy(6, 5, 6, 6, 5).sixes()
+
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([4, 4, 4, 5, 5], 0),
+        ([4, 4, 6, 5, 5], 6),
+        ([6, 5, 6, 6, 5], 18)
+    ],
+)
+def test_sum_sixes(dices, expected_result):
+    assert Yatzy.sum_sixes(*dices) == expected_result
 
 
-def test_one_pair():
-    assert 6 == Yatzy().score_pair(3, 4, 3, 5, 6)
-    assert 10 == Yatzy().score_pair(5, 3, 3, 3, 5)
-    assert 12 == Yatzy().score_pair(5, 3, 6, 6, 5)
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([3, 4, 3, 5, 6], 6),
+        ([4, 4, 6, 5, 5], 10),
+        ([1, 5, 6, 6, 5], 12)
+    ],
+)
+def test_one_pair(dices, expected_result):
+    assert Yatzy.one_pair(*dices) == expected_result
 
 
-def test_two_pair():
-    assert 16 == Yatzy().two_pair(3, 3, 5, 4, 5)
-    assert 18 == Yatzy().two_pair(3, 3, 6, 6, 6)
-    assert 0 == Yatzy().two_pair(3, 3, 6, 5, 4)
+@pytest.mark.parametrize(
+    "dices, expected_result",
+    [
+        ([3, 3, 5, 4, 5], 16),
+        ([4, 4, 6, 5, 5], 18),
+        ([3, 3, 6, 5, 4], 0)
+    ],
+)
+def test_two_pair(dices, expected_result):
+    assert Yatzy.two_pair(*dices) == expected_result
+
 
 
 def test_three_of_a_kind():
