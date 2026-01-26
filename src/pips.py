687fc2ec166eeaf7d80bbe1177a56132529b6f24
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-01-26 13:32:50 +0100

    style(Pips): stylize test

--- src/pips.py
+++ src/pips.py
@@ -35,15 +35,31 @@ class Combinations(Enum):
 
 
 if __name__ == "__main__":
+    from colorama import Fore
+    print(Fore.YELLOW + "Lista de 'clave-valor'" + Fore.RESET)
     print(list(Pips))
+
+    print(Fore.YELLOW + "Formas de obtener el atributo de la clase" + Fore.RESET)
+    print(Fore.YELLOW + "Por valor" + Fore.RESET)
     print(Pips(1))
-    print(Pips["ONE"]) 
+
+    print(Fore.YELLOW + "Por nombre" + Fore.RESET)
+    print(Pips["ONE"])
+
+    print(Fore.YELLOW + "Por atributo" + Fore.RESET)
     print(Pips.ONE)
+
+    print(Fore.YELLOW + "Devuelve el nobre de la constante en la clase" + Fore.RESET)
     print(Pips.ONE.name)
+
+    print(Fore.YELLOW + "Devuelve el valor de la constante en la clase" + Fore.RESET)
     print(Pips.ONE.value)
+
+    print(Fore.YELLOW + "Itera sobre los miembros de la clase y devuelve sus valores" + Fore.RESET)
     for number in Pips.__members__.values():
         print(number._value_)
 
+    print(Fore.YELLOW + "Llama las funciones de la clase" + Fore.RESET)
     print(Pips.values())
-    print(Pips.reversedValues())
+    print(list(Pips.reversedValues()))
     print(Pips.minus(Pips.FIVE))
